822c16b1c4c2adc59a0f8a452c25726c068dd07e
Francisco Mackinlay <franmackinlay@gmail.com>
2021-05-29 20:54:12 +0200

    IMPROVE, sports service

--- src/services/SportsSrv.js
+++ src/services/SportsSrv.js
@@ -1,17 +1,23 @@
 import axios from 'axios';
 
-const API_URL = 'https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4335';
+const API_URL = 'https://www.thesportsdb.com/api/v1/json/1';
 
 const SportsSrv = {
   getTeams: async () => {
     const { data: { teams } } = await axios({
       method: 'get',
-      url: API_URL,
+      url: `${API_URL}/lookup_all_teams.php?id=4335`,
     });
     return teams;
   },
-  getTeamPlayers: async () => {
+  getTeamDetails: async (id) => {
+    const { data: { teams } } = await axios({
+      method: 'get',
+      url: `${API_URL}/lookupteam.php?id=${id}`,
+    });
 
+    const response = Array.isArray(teams) ? teams[0] : teams;
+    return response;
   },
   getPlayer: async () => {
 
