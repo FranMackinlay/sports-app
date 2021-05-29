1bbb71662548276ecd161b9f23e4d74e7b8616e1
Francisco Mackinlay <franmackinlay@gmail.com>
2021-05-29 23:45:43 +0200

    IMPROVE, team card to support players variant

--- src/components/TeamCardComponent/TeamCardComponent.tsx
+++ src/components/TeamCardComponent/TeamCardComponent.tsx
@@ -3,16 +3,16 @@ import { Card } from '../../interfaces/interfaces';
 import { Link } from 'react-router-dom';
 import './TeamCardComponent.css';
 
-const TeamCard: FC<Card> = ({ cardData }) => {
+const TeamCard: FC<Card> = ({ team, player }) => {
 
   return (
-    <li className="card-li fm-mr-2 fm-mb-2">
+    <li className={`card-li fm-mr-2 fm-mb-2 ${player?.idPlayer ? 'variant-player' : 'variant-team'}`}>
       <div className="card fm-p-2">
-        <img className="card-img-top fm-maw50 fm-m-auto" src={cardData.strTeamBadge} alt="Team Badge" />
+        <img className="card-img-top fm-maw50 fm-m-auto" src={player ? player.strCutout : team?.strTeamBadge} alt="Team Badge" />
         <div className="card-body p-0 fm-df fm-jucb fm-fldc">
-          <h5 className="card-title text-center fm-w100 fm-df fm-global-center">{cardData.strAlternate}</h5>
-          <p className="card-text">{cardData.strDescriptionEN}</p>
-          <Link to={`/team/${cardData.idTeam}`} className="btn btn-primary">View team</Link>
+          <h5 className="card-title text-center fm-w100 fm-df fm-global-center">{player ? player.strPlayer : team?.strAlternate}</h5>
+          <p className="card-text">{player ? player.strPosition : team?.strDescriptionEN}</p>
+          <Link to={`${player ? `/player/${player.idPlayer}` : `/team/${team?.idTeam}`}`} className="btn btn-primary">{`${player ? 'View player' : 'View team'}`}</Link>
         </div>
       </div>
     </li>
