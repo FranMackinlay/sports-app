b1b621e292655b42497f8eac8fabb81b8a4c8c64
Francisco Mackinlay <franmackinlay@gmail.com>
2021-05-29 19:42:38 +0200

    ADD, sports service

---
+++ src/services/SportsSrv.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const API_URL = 'https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4335';
+
+const SportsSrv = {
+  getTeams: async () => {
+    const { teams } = await axios({
+      method: 'get',
+      url: API_URL,
+    });
+    return teams;
+  },
+  getTeamPlayers: async () => {
+
+  },
+  getPlayer: async () => {
+
+  },
+  createPlayer: async () => {
+
+  },
+  removePlayerFromTeam: async () => {
+
+  },
+}
+
+export default SportsSrv;
