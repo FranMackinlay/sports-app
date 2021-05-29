3bfd373c9db0fa7a5c3174ada4f29b50116c007f
Francisco Mackinlay <franmackinlay@gmail.com>
2021-05-29 20:27:26 +0200

    ADD, card component

---
+++ src/components/CardComponent/CardComponent.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+import { Team } from '../../interfaces/interfaces';
+import { Link } from 'react-router-dom';
+import './CardComponent.css';
+
+interface Card {
+  cardData: Team;
+}
+
+const Card: FC<Card> = ({ cardData }) => {
+
+  return (
+    <li className="card-li fm-mr-2 fm-mb-2">
+      <div className="card fm-p-2">
+        <img className="card-img-top fm-maw50 fm-m-auto" src={cardData.strTeamBadge} alt="Team Badge" />
+        <div className="card-body p-0">
+          <h5 className="card-title fm-df fm-aliic">{cardData.strAlternate}</h5>
+          <p className="card-text">{cardData.strDescriptionEN}</p>
+          <Link to={`/team/${cardData.idTeam}`} className="btn btn-primary">View team</Link>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default React.memo(Card);
