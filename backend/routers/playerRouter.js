929cea01e7ec5f64f5b9534b1c28d468a7e39158
Francisco Mackinlay <franmackinlay@gmail.com>
2021-05-30 01:58:38 +0200

    ADD, upsert player route

--- backend/routers/playerRouter.js
+++ backend/routers/playerRouter.js
@@ -2,15 +2,32 @@ import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import players from '../data.js';
 
+let playersTmp = players;
 
 const playerRouter = express.Router();
 
 playerRouter.get('/:idPlayer', expressAsyncHandler(async (req, res) => {
   const idPlayer = req.params.idPlayer;
 
-  const player = players.find(player => player.idPlayer === idPlayer);
+  const player = playersTmp.find(player => player.idPlayer.toString() === idPlayer);
 
   res.status(200).send({ player });
+}));
+
+playerRouter.put('/:idPlayer', expressAsyncHandler(async (req, res) => {
+  const idPlayer = req.params.idPlayer;
+  const { newPlayer } = req.body;
+  let upsertedPlayer;
+
+  playersTmp = players.map(player => {
+    if (player.idPlayer === idPlayer) {
+      player = newPlayer
+      upsertedPlayer = player;
+    };
+    return player;
+  });
+
+  res.status(200).send({ upsertedPlayer });
 }));
 
 export default playerRouter;
